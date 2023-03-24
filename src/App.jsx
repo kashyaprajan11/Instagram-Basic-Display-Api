9af1307e64aa5033fca9aad70360f86f9637be2c
kashyaprajan11 <rkashyap251@gmail.com>
2023-03-24 13:13:02 +0530

    Using material ui now. Checking the deployment as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Box, Stack, Button, Typography, TextField } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App">Hello world</div>;
+  return (
+    <Box>
+      <TextField />
+      <Button>Send me to my instagram</Button>
+    </Box>
+  );
 }
 
 export default App;
