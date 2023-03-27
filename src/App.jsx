0b948283fb043bd4ff8d8ab3b09c4fa645282aca
kashyaprajan11 <rkashyap251@gmail.com>
2023-03-27 11:22:10 +0530

    Redirecting to instagram authorize thing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState, useEffect } from "react";
 import { Box, Stack, Button, Typography, TextField } from "@mui/material";
 
 function App() {
+  const getInstagramAccessCode = () => {
+    let code = "";
+    window.location.replace(
+      "https://api.instagram.com/oauth/authorize?client_id=612567580316276&redirect_uri=https://instagram-basic-display-api.vercel.app/&scope=user_profile,user_media&response_type=code"
+    );
+    console.log(window.location.href);
+  };
+
   return (
     <Box>
       <TextField />
-      <Button>Send me to my instagram</Button>
+      <Button onClick={getInstagramAccessCode}>Send me to my instagram</Button>
     </Box>
   );
 }
